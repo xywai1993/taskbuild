68c3f715edb6cb2414e124a30bd3bf6d5fcdece6
yiper <yiper.fan@gmail.com>
2021-03-18 13:22:12 +0800

    切换为esmodule

---
+++ dist/index.d.ts
@@ -0,0 +1,63 @@
+export interface customFile {
+    name: string;
+    basename: string;
+    from: string;
+    release: string;
+    extname: string;
+    isDir: boolean;
+}
+interface settings {
+    taskList: [{
+        taskName: 'qiniuUpload';
+        params: qiniuUploadTaskParams;
+    } | {
+        taskName: 'fileMove' | 'htmlMove';
+        params: MoveTaskParams;
+    } | {
+        taskName: 'cleanDir';
+        params: cleanDirTaskParams;
+    }];
+}
+/**
+ * 主函数
+ * @param {object} settings
+ */
+declare function startTask(settings: settings): void;
+interface cleanDirTaskParams {
+    root: string;
+    rmSelf: boolean;
+}
+export interface qiniuUploadTaskParams {
+    /**
+     * 要上传的文件目录，完整路径
+     */
+    root: string;
+    /**
+     *  https://image.douba.cn/${publicName}/file
+     */
+    publicName: string;
+    /**
+     * 七牛accessKey
+     */
+    accessKey: string;
+    /**
+     * 七牛secretKey
+     */
+    secretKey: string;
+}
+interface MoveTaskParams {
+    /**
+     * 在列表中的文件后缀将被转移
+     * @default ['html']
+     */
+    extname?: string[];
+    /**
+     * 类似fileMove
+     */
+    root: string;
+    /**
+     * 类似fileMove
+     */
+    deployTo: string;
+}
+export { startTask };
