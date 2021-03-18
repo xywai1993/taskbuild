3c8e997cc835b6b3a4ed15efbc4b0f61f204917c
yiper <yiper.fan@gmail.com>
2021-03-18 16:52:33 +0800

    up

--- dist/test/index.test.js
+++
@@ -1,7 +0,0 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-console.log(dirname(fileURLToPath(import.meta.url)));
-test('__dirname', () => {
-    // const p = dirname(fileURLToPath(import.meta.url));
-    // expect(__dirname(import.meta)).tobe;
-});
